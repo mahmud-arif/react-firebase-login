474a44a549a8662a2c090dfe44aa3100cced187c
mahmud <mahmudarif1175@gmail.com>
2020-01-29 06:43:32 +0600

    remove: unneccesary code

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -99,17 +99,3 @@ export const verifyAuth = () => (dispatch) => {
 };
 
 
-// export const loginUser = (email, password) => {
- 
-//     return async function(dispatch){
-//         const user = await firebase.login(email, password).catch(err => console.log(err));
-        
-//         if(user){
-            
-//             dispatch({type: "LOGIN_USER", payload: user});
-//             return user;
-//         }        
-//     }
- 
-
-// }
